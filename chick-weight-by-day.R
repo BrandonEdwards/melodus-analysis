f142142dd1523b746764367c5c27f3f07f0dc6a1
Brandon Edwards <brandon.edwards144@gmail.com>
2017-07-05 10:51:40 -0400

    Read in files and bind together and plot by run

--- chick-weight-by-day.R
+++ chick-weight-by-day.R
@@ -19,23 +19,34 @@ library(ggplot2)
 # Read Data
 ################################
 
-d1 <- read.csv(file.choose(), header = FALSE)
-d1$Humans <- TRUE
-d2 <- read.csv(file.choose(), header = FALSE)
-d2$Humans <- FALSE
+# Create empty data frame to bind data to
+data <- data.frame(Day=integer(),
+                 Num.Chicks=integer(),
+                 c1=double(),
+                 c2=double(),
+                 c3=double(),
+                 c4=double(),
+                 Mean.Weight=double(),
+                 Run=integer(),
+                 stringsAsFactors=FALSE)
 
-names(d1)<-c("Day", "Num.Chicks", "c1", "c2", "c3", "c4", "Mean.Weight", "Humans")
-names(d2)<-c("Day", "Num.Chicks", "c1", "c2", "c3", "c4", "Mean.Weight", "Humans")
-
-data <- rbind(d1, d2)
+# Number of files to read in
+numFiles <- 8
+for (i in 1:numFiles)
+{
+  temp <- read.csv(paste("testResultsNoHumans", i, ".csv", sep=""), header = FALSE)
+  temp$Run <- i
+  names(temp)<-c("Day", "Num.Chicks", "c1", "c2", "c3", "c4", "Mean.Weight", "Run")
+  data <- rbind(data, temp)
+}
 
 ################################
 # Plot
 ################################
-ggplot(data = data, aes(x = Day, y = Weight (g), group=Humans)) + 
-  geom_line(aes(y = data[3], color = Humans)) + 
-  geom_line(aes(y = data[4], colour = Humans)) + 
-  geom_line(aes(y = data[5], colour = Humans)) + 
-  geom_line(aes(y = data[6], colour = Humans)) + 
+ggplot(data = data, aes(x = Day, y = Weight (g), group=Run)) + 
+  geom_line(aes(y = data[3], color = Run)) + 
+  geom_line(aes(y = data[4], colour = Run)) + 
+  geom_line(aes(y = data[5], colour = Run)) + 
+  geom_line(aes(y = data[6], colour = Run)) + 
   geom_abline(mapping = NULL, data = NULL, colour = "black", size = 1, slope = 1.375, intercept = 3.625) + 
   annotate("text", x = 13, y = 32.5, label = "Expected growth curve (Martens and Goossen 2008)")
