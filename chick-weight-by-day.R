c0053ff1dc9195c3e8358f0c0f934eed91a45781
Brandon Edwards <brandon.edwards144@gmail.com>
2017-07-06 17:41:21 -0400

    Read in files from input directory and output plot to pdf

--- chick-weight-by-day.R
+++ chick-weight-by-day.R
@@ -13,6 +13,9 @@ remove(list = ls())
 ################################
 # Import Libraries
 ################################
+
+#install.packages("ggplot2")
+
 library(ggplot2)
 
 ################################
@@ -27,32 +30,42 @@ data <- data.frame(Day=integer(),
                  Run=integer(),
                  stringsAsFactors=FALSE)
 
-dateDirs <- list.files("model-output")
+dateDirs <- list.files("input")
+run <- 0
 for (i in 1:length(dateDirs))
 {
-  runDirs <- list.files(paste("model-output/", dateDirs[i], sep=""))
+  runDirs <- list.files(paste("input/", dateDirs[i], sep=""))
   for (j in 1:length(runDirs))
   {
-    temp <- data.frame(read.csv(paste("model-output/", dateDirs[i], "/", runDirs[j], "/chickWeights.csv", sep="")))
-    
+    temp <- data.frame(read.csv(paste("input/", dateDirs[i], "/", runDirs[j], "/chickWeights.csv", sep="")))
+    temp$Run <- run
+    run <- run + 1
+    names(temp)<-c("Day", "Num.Chicks", "Weight", "Mean.Weight", "Run")
+    data <- rbind(data, temp)
   }
 }
 
 
 # Number of files to read in
-numFiles <- 2
-for (i in 0:numFiles)
-{
-  temp <- data.frame(read.csv(paste("05-07-2017/run", i, "/chickWeights.csv", sep="")))
-  temp$Run <- i
-  names(temp)<-c("Day", "Num.Chicks", "Weight", "Mean.Weight", "Run")
-  data <- rbind(data, temp)
-}
+#numFiles <- 2
+#for (i in 0:numFiles)
+#{
+#  temp <- data.frame(read.csv(paste("05-07-2017/run", i, "/chickWeights.csv", sep="")))
+#  temp$Run <- i
+#  names(temp)<-c("Day", "Num.Chicks", "Weight", "Mean.Weight", "Run")
+#  data <- rbind(data, temp)
+#}
 
 ################################
 # Plot
 ################################
-ggplot(data = data, aes(x = Day, y = Weight (g), group=Run)) + 
+
+pdf("chickWeight.pdf")
+
+p <- ggplot(data = data, aes(x = Day, y = Weight (g), group=Run)) + 
   geom_line(aes(y = Mean.Weight, color = Run)) +
   geom_abline(mapping = NULL, data = NULL, colour = "black", size = 1, slope = 1.375, intercept = 3.625) + 
   annotate("text", x = 13, y = 32.5, label = "Expected growth curve (Martens and Goossen 2008)")
+
+print(p)
+dev.off()
