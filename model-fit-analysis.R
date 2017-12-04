a9da1833992f247c8392fd917c0e621a8e79b7a8
Brandon Edwards <brandon.edwards144@gmail.com>
2017-12-04 12:45:24 -0500

    Added statistical tests

--- model-fit-analysis.R
+++ model-fit-analysis.R
@@ -251,4 +251,22 @@ hist(I.Base.RelBias, xlab = "Relative Bias (%)", main = "Relative Bias of\nInfle
 hist(K.Base.RelBias, xlab = "Relative Bias (%)", main = "Relative Bias of\nGrowth Rate Constant (K)")
 dev.off()
 
+#####################################
+# Statistical Tests
+#####################################
 
+# Simualated Day 31 vs Target
+day31Data <- baseData[ which(baseData$Day == 31), ]
+chars <- capture.output(print(t.test(day31Data$Mean.Weight, mu = 53.4*exp(-exp(-0.084*(31-11.271))))))
+writeLines(chars, con = file("output/fitAnalysis/tests/meanSimDay31VsTarget.txt"))
+
+
+# Mean Simulated Growth Rate Constant vs. Target
+chars <- capture.output(print(t.test(baseEstimates$K.Est, mu = 0.084)))
+writeLines(chars, con = file("output/fitAnalysis/tests/meanSimGrowthRateVsTarget.txt"))
+
+# Mean Simulated Mass Asymptote vs. Target
+chars <- capture.output(print(t.test(baseEstimates$A.Est, mu = 53.3)))
+writeLines(chars, con = file("output/fitAnalysis/tests/meanSimMassAsympVsTarget.txt"))
+
+closeAllConnections()
